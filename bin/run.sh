267519da971e09400c6e21b489d4d0bdaffc4f70
Rachel Brindle <you@subluminal.net>
2018-12-28 23:28:16 -0800

    Update dependencies

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-bundle exec rake kindlegen:download:linux && bundle exec rails s -b localhost -p $PORT -e production
+set -e
+
+bundle install
+bundle exec rake kindlegen:download:linux
+bundle exec rails s -b localhost -p $PORT -e production
